5d36a034ba3430c4517eed6db1340b304f19b706
esteban-vargasbernal <esteban.vargas.bernal@asu.edu>
2025-06-16 15:31:56 -0700

    Fixed plotting files

---
+++ plotting_bars_scenarios.R
@@ -0,0 +1,89 @@
+#setwd("/Users/estebanvargasbernal/Documents/github/symbiont-project")
+
+library(arrangements)
+library(matrixcalc)
+library(ggplot2)
+library(parallel)
+ncores <- detectCores() - 1
+RNGkind("L'Ecuyer-CMRG")
+library(latex2exp)
+library(sfsmisc)
+library(nnls)
+library(tidyverse)
+library(dplyr)
+
+
+df <-read.csv(file = 'Data/df_small_N_1000_3.csv', header = TRUE,  sep = ",")
+
+n_split <- 4
+
+
+df_tmp <- df  %>%
+group_by(e_cell_0, e_sym_0, sim) %>%
+mutate(theta_sym = first(theta_sym), theta_cell = first(theta_cell))
+    
+df_tmp <- df_tmp %>%
+mutate(theta_sym = as.character(round(theta_sym,2)), 
+theta_cell = as.character(round(theta_cell,2))) 
+
+df_tmp <- df_tmp %>%
+    transform(theta_sym = factor(theta_sym, levels = c("0.5","0.17","-0.17","-0.5") ),
+    theta_cell = factor(theta_cell, levels = c("-0.5","-0.17","0.17","0.5") ),
+    scenario = factor(scenario, levels = c("Mutualism",  'Predator-prey', 'Parasitism', 'Competition')))
+
+### Plot duration in each sscenario
+
+for(epsilon_cell_0 in c(0.001, 0.00001)){
+    for(epsilon_sym_0 in c(0.001, 0.00001)){
+
+        df_plot <- df_tmp  %>% filter(epsilon_cell == epsilon_cell_0, epsilon_sym == epsilon_sym_0) 
+
+        plot_cor <-  df_plot %>%
+        group_by(e_cell_0, e_sym_0, sim) %>% 
+        ggplot( aes(x = scenario)) +
+        geom_bar(aes( fill = scenario), stat = 'count') + 
+        scale_fill_manual(values = c( "red", "blue","green","black" )) +
+        facet_wrap(~ theta_sym + theta_cell, labeller = labeller(
+                    theta_cell = ~ paste("Theta host: ", .),
+                    theta_sym = ~ paste("Theta_sym: ", .) ,nrow = n_split)) +
+        labs(x = 'Scenario', y = 'Number of generations in each scenario', title = paste0('Distributions of time spent in each scenario over different simulations for 10000 generations, \n epsilon_host = ', epsilon_cell_0,', epislon_sym = ',epsilon_sym_0, ', 3 simulations for each parameter combination')) +
+        labs(fill = "Scenario")+
+        theme(axis.text = element_text(size = 15), plot.title = element_text(size = 14),
+                axis.title.x = element_text(size = 16), axis.title.y = element_text(size = 16), # nolint
+                legend.text = element_text(size = 15), axis.text.x=element_blank(), axis.ticks.x = element_blank())
+
+        #plot_cor
+        ggsave(plot_cor, file = paste0('Figures/distribution_duration_each_scenario_epsilon_cell_',epsilon_cell_0,'_epislon_sym_',epsilon_sym_0,'.pdf'))
+    }
+}
+
+df_tmp_2 <- df_tmp %>% group_by(theta_cell,theta_sym,sim, epsilon_cell, epsilon_sym) %>%
+              summarise(scenario_end_short = factor(first(scenario_end), levels = c("Mutualism",  'Predator-prey', 'Parasitism', 'Competition') ))
+
+### Plot final scenario 
+
+for(epsilon_cell_0 in c(0.001, 0.00001)){
+    for(epsilon_sym_0 in c(0.001, 0.00001)){
+
+        df_plot_2 <- df_tmp_2  %>% filter(epsilon_cell == epsilon_cell_0, epsilon_sym == epsilon_sym_0) 
+
+        plot_cor <-  df_plot_2 %>%
+        group_by(theta_cell, theta_sym, sim) %>% 
+        ggplot( aes(x = scenario_end_short)) +
+        #geom_bar(aes( fill = scenario), position = 'fill') + 
+        geom_bar(aes( fill = scenario_end_short), stat = 'count') + 
+            #geom_bar(aes(y = ..prop.., group = 1, fill = scenario), stat =  'count') +
+        scale_fill_manual(values = c( "red", "blue","green","black" )) +
+        facet_wrap(~ theta_sym + theta_cell, labeller = labeller(
+                    theta_cell = ~ paste("Theta host: ", .),
+                    theta_sym = ~ paste("Theta_sym: ", .) ,nrow = n_split)) +
+        labs(x = 'Scenario', y = 'Number of generations in each scenario', title = paste0('Distributions of final scenario over different simulations for 10000 generations, \n epsilon_host = ', epsilon_cell_0,', epislon_sym = ',epsilon_sym_0, ', 3 simulations for each parameter combination')) +
+        labs(fill = "Scenario")+
+        theme(axis.text = element_text(size = 15), plot.title = element_text(size = 14),
+                axis.title.x = element_text(size = 16), axis.title.y = element_text(size = 16), # nolint
+                legend.text = element_text(size = 15), axis.text.x=element_blank(), axis.ticks.x = element_blank())
+
+        ggsave(plot_cor, file = paste0('Figures/final_scenario_epsilon_cell_',epsilon_cell_0,'_epislon_sym_',epsilon_sym_0,'.pdf'))
+    }
+}
+
---
+++ plotting_theta_trajectories.py
@@ -0,0 +1,100 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import math
+import copy
+import random
+import pandas as pd
+
+import functions_symbionts as sym
+
+import sys
+
+from collections import Counter
+
+
+
+#df_all = pd.read_csv('Data/df_N_1000.csv')
+
+df_all = pd.read_csv('Data/df_small_N_1000_3.csv')
+
+
+scenarios = ['Mutualism', 'Predator-prey', 'Parasitism', 'Competition']
+colors = {'Mutualism':"red",'Predator-prey':"blue", 'Parasitism':"green", 'Competition': "black"}
+
+N_sim = 3
+N_gen = 10000
+
+epsilon_cell_large = 0.001
+epsilon_sym_large = 0.001 #u_sym*L
+
+epsilon_cell_small = 0.00001
+epsilon_sym_small = 0.00001 #u_sym*L
+
+n_split = 4
+
+e_for_symbionts_ls = np.linspace(-0.75,0.25,n_split)
+e_for_cells_ls = np.linspace(-0.75,0.25,n_split)
+
+
+
+h_for_cells_ls = n_split*[0.25]
+h_for_symbionts_ls = n_split*[0.25]
+
+
+
+
+for epsilon_cell in [epsilon_cell_large, epsilon_cell_small]:
+    
+    for epsilon_sym in [epsilon_sym_large, epsilon_sym_small]:
+
+        for e_sym in e_for_symbionts_ls:
+
+                for e_cell in e_for_cells_ls:
+                    
+                    for j in np.arange(N_sim):
+
+                        df = df_all[ np.logical_and(df_all['epsilon_cell']==epsilon_cell, df_all['epsilon_sym']==epsilon_sym)]
+                        df_tmp = df[np.round(df['e_sym_0'],2) == np.round(e_sym,2)]
+                        df_tmp = df_tmp[np.round(df_tmp['e_cell_0'],2)==np.round(e_cell,2)]
+                        df_tmp = df_tmp[df_tmp['sim']==j]
+
+                        theta_cell = df_tmp["theta_cell"].iloc[0]
+                        theta_sym = df_tmp['theta_sym'].iloc[0]
+
+                        scenario_start = df_tmp['scenario_start'].iloc[0]
+                        scenario_end = df_tmp['scenario_end'].iloc[0]
+                       
+                        x = df_tmp[["theta_cell"]]
+                        y = df_tmp[['theta_sym']]
+                        
+                        if j == 0:
+                            if (e_sym, e_cell) in [(np.max(e_for_symbionts_ls), np.max(e_for_cells_ls)), 
+                                                (np.min(e_for_symbionts_ls), np.max(e_for_cells_ls)),
+                                                (np.max(e_for_symbionts_ls), np.min(e_for_cells_ls)),
+                                                (np.min(e_for_symbionts_ls), np.min(e_for_cells_ls))]:
+                                plt.plot(x.iloc[0],y.iloc[0], 'D', color = colors[scenario_start], label = scenario_start)
+                                plt.plot(x,y, '-', alpha = 0.1, color = colors[scenario_start])
+                            else:
+                                plt.plot(x.iloc[0],y.iloc[0], 'D', color = colors[scenario_start])
+                                plt.plot(x,y, '-', alpha = 0.1, color = colors[scenario_start])
+                        else:
+                            plt.plot(x,y, '-', alpha = 0.1, color = colors[scenario_start])
+                        
+                        plt.plot(x.iloc[-1], y.iloc[-1],'*',alpha = 0.5, linewidth =3,color = colors[scenario_start])
+                        
+
+
+        plt.hlines(y = 0, xmin=-1, xmax =2, color = "black")
+        plt.vlines(x = 0, ymin =-1, ymax = 1.2, color = "black")
+        plt.xlim(-1,2)
+        plt.ylim(-1,1.2)
+
+        plt.grid()
+        plt.xlabel(r'$\theta_{\text{host}} = e_{\text{sym}} + h_{\text{host}}$', fontsize = 16)
+        plt.ylabel(r'$\theta_{\text{sym}} = e_{\text{host}} + h_{\text{sym}}$', fontsize = 16)
+        plt.title(r'$\epsilon_{\text{host}}$ = '+ str(epsilon_cell)+ r', $\epsilon_{\text{sym}} = $'+str(epsilon_sym), fontsize = 18)
+        plt.legend(bbox_to_anchor=(1.01, 1), borderaxespad=0, title = 'Scenario of the \n initial condition')
+        plt.tight_layout()
+        plt.savefig('Figures/theta_trajectories_epsilon_host_'+ str(epsilon_cell)+ '_epsilon_sym_'+str(epsilon_sym)+'.png')
+        plt.show()
